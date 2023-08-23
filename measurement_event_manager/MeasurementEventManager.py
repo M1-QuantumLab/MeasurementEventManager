a23ad97c476b9f5731cf66026212354058880bb4
Sam Wolski <wolski.samp@gmail.com>
2023-08-23 11:40:24 -0400

    Add basic ZeroMQ server

---
+++ measurement_event_manager/MeasurementEventManager.py
@@ -0,0 +1,8 @@
+'''
+The main MeasurementEventManager class.
+'''
+
+class MeasurementEventManager(object):
+    
+    def __init__(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+## Dependencies
+_requires = [ r for r in
+              open('requirements.txt', 'r')\
+              .read().split('\n') if len(r)>1 ]
+
+setuptools.setup(
+    name='MeasurementEventManager',
+    version="0.0.1",
+    description='Server for measurement queuing and processing',
+    author='Sam Wolski',
+    python_requires='>=2.7',
+    packages=["measurement_event_manager"],
+    install_requires=_requires,
+    scripts=[
+            "bin/run_mem",
+            ],
+)
