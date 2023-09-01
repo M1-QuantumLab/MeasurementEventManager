657323a896f95b8d5e92a432bfb95aed90488bf2
Sam Wolski <wolski.samp@gmail.com>
2023-09-01 12:03:46 -0400

    Fix incorrect measurement times when reconstructing from JSON
    When constructing from a JSON string including null values for start_time and stop_time, the setter functions would be called with a None argument, resulting in the intended default behvaiour of the function, ie. setting the time to the present time.
    This is incorrect in this case, as the parameter should not be changed from a None value!

--- measurement_event_manager/MeasurementParams.py
+++ measurement_event_manager/MeasurementParams.py
@@ -53,8 +53,13 @@ def _mp_from_json(json_dict):
         ## Create the object
         mp_obj = MeasurementParams(**mp_dict)
         ## Set the start and stop times
-        mp_obj.set_start_time(start_time)
-        mp_obj.set_stop_time(stop_time)
+        ## Calling the function with a None argument sets it to the current
+        ## time, so we want to avoid setting a time that's not real
+        ## There is probably a better way of doing this...
+        if start_time is not None:
+            mp_obj.set_start_time(start_time)
+        if stop_time is not None:
+            mp_obj.set_stop_time(stop_time)
         ## Return the final object with updated special params
         return mp_obj
     ## Handle any individual objects that require special parsing
