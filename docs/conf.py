be806dc9d4badc1db6f2d5a36eedfcee7921d8e9
Sam Wolski <wolski.samp@gmail.com>
2024-09-25 15:40:01 -0400

    Docs: Match versions

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@ project = 'MeasurementEventManager'
 copyright = '2023-2024, Sam Wolski'
 author = 'Sam Wolski'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.4'
+version = '0.4.2'
 
 # -- General configuration
 
